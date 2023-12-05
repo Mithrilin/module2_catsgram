8c008561f511983edbea6e14ff94422e95e66503
Roman <romanukyan86@yandex.ru>
2023-12-05 16:18:07 +0300

    feat: добавить метод findById в PostService

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exceptions.PostNotFoundException;
 import ru.yandex.practicum.catsgram.exceptions.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -30,5 +32,11 @@ public class PostService {
         }
         posts.add(post);
         return post;
+    }
+
+    public Post findById(int postId) {
+        return posts.stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst().orElseThrow(() -> new PostNotFoundException("Пост №" + postId + " не найден."));
     }
 }
